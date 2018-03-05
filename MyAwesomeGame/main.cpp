e7706bd2d55574d20a374ec29794dbd33033b6a0
DidacRomero <36265669+DidacRomero@users.noreply.github.com>
2018-03-05 01:57:13 +0100

    Added Simple Event
    You can now close the window when pressing any button.

--- MyAwesomeGame/main.cpp
+++ MyAwesomeGame/main.cpp
@@ -7,6 +7,7 @@ int main(int argc,char* argv[])
 {
 	SDL_Window *window=NULL;
 	SDL_Renderer* renderer=NULL;
+	SDL_Event checkEvents;
 
 	SDL_Init(SDL_INIT_VIDEO);
 
@@ -14,8 +15,8 @@ int main(int argc,char* argv[])
 	SDL_Rect redRect;
 	redRect.x = 1280/4;
 	redRect.y = 720/4;
-	redRect.w = 400;
-	redRect.h = 300;
+	redRect.w = 250;
+	redRect.h = 150;
 	
 	//Set the Window
 		window = SDL_CreateWindow(
@@ -29,19 +30,20 @@ int main(int argc,char* argv[])
 		//Set the Renderer
 		renderer = SDL_CreateRenderer(window, -1, 0);
 
-		int xspeed = 2, yspeed = 2;
-		
-		while (true)
+		int xspeed = 0, yspeed = 0;
+		bool exitLoop = false;
+	
+		while (exitLoop!=true)
 		{
 			//Set new position for the square
-			if (redRect.x <= 0)
-				xspeed = 1;
-			else if(redRect.x >= 880)
-				xspeed = -1;
-			else if(redRect.y <= 0)
-				yspeed = 1;
-			else if (redRect.y >= 420)
-				yspeed = -1;
+			if (SDL_PollEvent(&checkEvents)!=0)
+			{
+				if (checkEvents.type == SDL_KEYDOWN)
+				{
+					exitLoop = true;
+				}
+			
+			}
 
 			redRect.x += xspeed;
 			redRect.y += yspeed;
@@ -59,6 +61,22 @@ int main(int argc,char* argv[])
 		}
 		
 		SDL_DestroyRenderer(renderer);
+		renderer = NULL;
 		SDL_DestroyWindow(window);
+		window = NULL;
 	return 0;
 }
+
+
+
+
+/*
+if (redRect.x <= 0)
+xspeed = 1;
+else if(redRect.x >= 880)
+xspeed = -1;
+else if(redRect.y <= 0)
+yspeed = 1;
+else if (redRect.y >= 420)
+yspeed = -1;
+*/
